369f8ded89c0ad53fb46cff9a42b1398f2e1f050
mark-hkz <mark-hkz>
2024-02-28 20:03:30 +0800

    remove flutter_svg/parser.dart import, and fix unsupported param-default-value syntax

--- lib/image_render.dart
+++ lib/image_render.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/html_parser.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_svg/parser.dart';
 import 'package:html/dom.dart' as dom;
 
 typedef ImageSourceMatcher = bool Function(
@@ -26,7 +25,7 @@ ImageSourceMatcher dataUriMatcher(
     };
 
 ImageSourceMatcher networkSourceMatcher({
-  List<String> schemas: const ["https", "http"],
+  List<String> schemas = const ["https", "http"],
   List<String>? domains,
   String? extension,
 }) =>
